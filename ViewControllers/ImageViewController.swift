d7552b5ef051f3f07079ba3ded5869ff141cf82f
Artem Pavlov <pavlov.tema@gmail.com>
2021-09-26 18:32:47 +0500

    imageView added & image loaded from url

---
+++ ViewControllers/ImageViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  NasaApp
+//
+//  Created by iMac on 24.09.2021.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var activityIndicator: UIActivityIndicatorView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        activityIndicator.startAnimating()
+        activityIndicator.hidesWhenStopped = true
+        fetchImage()
+    }
+
+    private func fetchImage() {
+        guard let url = URL(string: Link.imageURL.rawValue) else {return}
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data, let response = response else {
+                print(error?.localizedDescription ?? "No error description")
+                return
+            }
+            print(response)
+            
+            guard let image = UIImage(data: data) else {return}
+            
+            DispatchQueue.main.async {
+            self.imageView.image = image
+            self.activityIndicator.stopAnimating()
+            }
+        }.resume()
+    }
+    
+}
+
--- ViewControllers/MainViewController.swift
+++ ViewControllers/MainViewController.swift
@@ -7,62 +7,68 @@
 
 import UIKit
 
+enum Link: String {
+    case imageURL = "https://apod.nasa.gov/apod/image/2109/RedSquare_Tuthill_960.jpg"
+    case pictureOfToday = "https://apod.nasa.gov/apod/astropix.html"
+    case marsRoverPhotos = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=DEMO_KEY"
+    case geomagneticStorm = "https://api.nasa.gov/DONKI/GST?startDate=yyyy-MM-dd&endDate=yyyy-MM-dd&api_key=DEMO_KEY"
+}
 
+enum UserAction: String, CaseIterable {
+    case downloadImage = "Today's NASA photo"
+    case pictureOfToday = "Info about today's photo"
+    case marsRoverPhotos = "Mars Rover photos"
+    case geomagneticStorm = "Geomagnetic Storm info"
+}
 
 class MainViewController: UICollectionViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        
-        
-    }
-
- 
-
+    
+    let userActions = UserAction.allCases
+    
     // MARK: UICollectionViewDataSource
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        
-        return 1
+        userActions.count
     }
-
+    
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! UserActionCell
-    
         
-    
+        cell.userActionLabel.text = userActions[indexPath.item].rawValue
+        
         return cell
     }
-
-    // MARK: UICollectionViewDelegate
-
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
     
+    // MARK: UICollectionViewDelegate
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        let userAction = userActions[indexPath.item]
+        
+        switch userAction {
+        case .downloadImage: performSegue(withIdentifier: "showImage", sender: nil)
+        case .pictureOfToday: pictureOfTodayButtonPressed()
+        case .marsRoverPhotos: marsRoverPhotosButtonPressed()
+        case .geomagneticStorm: geomagneticStormButtonPressed()
+        }
     }
-    */
-
+}
+// MARK: - Networking
+extension MainViewController {
+    private func pictureOfTodayButtonPressed() {
+        
+    }
+    
+    private func marsRoverPhotosButtonPressed() {
+        
+    }
+    
+    private func geomagneticStormButtonPressed() {
+        
+    }
+    
+}
+// отображение cell под разные экраны
+extension MainViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        CGSize(width: UIScreen.main.bounds.width - 48, height: 100)
+    }
 }
